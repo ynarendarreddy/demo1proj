d1ab5b66cade5dc1845dc04b3ae07d15eb73419b
ynarendarreddy <39728479+ynarendarreddy@users.noreply.github.com>
2018-05-29 16:36:16 +0530

    adding new class Author

--- sessionapp/models.py
+++ sessionapp/models.py
@@ -2,6 +2,9 @@ from django.db import models
 class book(models.Model):
     bookid=models.IntegerField(primary_key=True)
     bookname=models.CharField(max_length=20)
+class author(models.Model):
+     authorid=models.IntegerField(primary_key=True)
+    authorname=models.CharField(max_length=20)
 
 
 
