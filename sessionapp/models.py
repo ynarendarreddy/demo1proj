438e723ac7763ac0abcefb30bb5747f6e702036b
ynarendarreddy <yanumulanarendar22@gmail.com>
2018-05-29 16:32:20 +0530

    commited

--- sessionapp/models.py
+++ sessionapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class book(models.Model):
+    bookid=models.IntegerField(primary_key=True)
+    bookname=models.CharField(max_length=20)
+
+
 
 # Create your models here.
